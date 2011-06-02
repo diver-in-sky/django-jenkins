8cb607620baef899de46c9d7f4d8b3c883dcec24
Andrew <tereskin@gmail.com>
2011-06-02 17:59:43 +0400

    add --tests-excludes option to django_tests.py

--- django_jenkins/tasks/django_tests.py
+++ django_jenkins/tasks/django_tests.py
@@ -3,6 +3,7 @@
 """
 Build suite with normal django tests
 """
+from optparse import make_option
 from django.test.simple import build_suite, build_test
 from django.db.models import get_app, get_apps
 from django.conf import settings
@@ -11,8 +12,14 @@ from django_jenkins.tasks import BaseTask
 
 
 class Task(BaseTask):
+    option_list = [make_option("--tests-exclude", action="append",
+                               default=[], dest="tests_excludes",
+                               help="App name to exclude")]
     def __init__(self, test_labels, options):
         super(Task, self).__init__(test_labels, options)
+        self.exclude_apps = []
+        for appname in options['tests_excludes']:
+            self.exclude_apps.append(appname)
         if not self.test_labels:
             if hasattr(settings, 'PROJECT_APPS') and not options['test_all']:
                 self.test_labels = [app_name.split('.')[-1] for app_name in settings.PROJECT_APPS]
@@ -20,14 +27,16 @@ class Task(BaseTask):
     def build_suite(self, suite, **kwargs):
         if self.test_labels:
             for label in self.test_labels:
-                if '.' in label:
-                    suite.addTest(build_test(label))
-                else:
-                    try:
-                        app = get_app(label)
-                        suite.addTest(build_suite(app))
-                    except ImproperlyConfigured:
-                        pass
+                if label not in self.exclude_apps:
+                    if '.' in label:
+                        suite.addTest(build_test(label))
+                    else:
+                        try:
+                            app = get_app(label)
+                            suite.addTest(build_suite(app))
+                        except ImproperlyConfigured:
+                            pass
         else:
             for app in get_apps():
-                suite.addTest(build_suite(app))
+                if app not in self.exclude_apps:
+                    suite.addTest(build_suite(app))
